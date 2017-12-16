cccdf394dacbb285b3bc0b9fbd6e45bb3e7c69ca
CharleyXu <xucharlie1993@gmail.com>
2017-12-16 23:27:44 +0800

    update springboot project

---
+++ src/main/java/com/xu/springredlock/SpringBootRedlockApplication.java
@@ -0,0 +1,12 @@
+package com.xu.springredlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedlockApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootRedlockApplication.class, args);
+  }
+}
